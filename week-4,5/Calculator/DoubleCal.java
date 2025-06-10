c405a59d5d613ad1020a1a6e6e829038da291eec
Adithya Pathakoti <151919367+AdithyaPathakoti@users.noreply.github.com>
2025-06-10 22:00:46 +0530

    Create DoubleCal.java

---
+++ week-4,5/Calculator/DoubleCal.java
@@ -0,0 +1,16 @@
+package Calculator;
+public  class DoubleCal{
+    public double add(double a,double b){
+        return a+b;
+    }
+    public double sub(double a,double b){
+        return a-b;
+    }
+    public double mul(double a,double b){
+        return a*b;
+    }
+    public double div(double a,double b){
+        return a/b;
+    }
+}
+//javac DoubleCal.java

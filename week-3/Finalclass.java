d4a05901c3ec916b832c95c4b198408f5b2d7677
Adithya Pathakoti <151919367+AdithyaPathakoti@users.noreply.github.com>
2025-06-10 21:53:34 +0530

    Create Finalclass.java

---
+++ week-3/Finalclass.java
@@ -0,0 +1,14 @@
+final class bike1{
+    
+}
+class Finalclass extends bike1 {
+    void run(){
+        System.out.println("HELLo");
+    }
+    public static void main(String[] args) {
+        Finalclass n=new Finalclass();
+        n.run();
+    }
+}
+//javac Finalclass.java
+//output will be error

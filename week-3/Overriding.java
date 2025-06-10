a198850d6722f9a0239d9a15fb00c4069f30c585
Adithya Pathakoti <151919367+AdithyaPathakoti@users.noreply.github.com>
2025-06-10 21:50:52 +0530

    Create Overriding.java

---
+++ week-3/Overriding.java
@@ -0,0 +1,16 @@
+class vehicle{
+    void run(){
+        System.out.println("Vehicle is running");
+    }
+}
+class bike extends vehicle{
+    void run(){
+        System.out.println("Bike is running");
+    }
+    public static void main(String[] args) {
+        bike obj=new bike();
+        obj.run();
+    }
+}
+//javac Overriding.java
+//java bike
